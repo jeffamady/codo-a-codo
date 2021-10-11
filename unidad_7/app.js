5dd59a66d843e23a9f2ae3339bc68ce1d8ba00fb
jeffamady <jeff02.jn@gmail.com>
2021-10-10 21:42:45 -0300

    js unit 1 done

---
+++ unidad_7/app.js
@@ -0,0 +1,5 @@
+var primera = 40;
+var segunda = 16;
+var sumaTotal = primera + segunda;
+
+document.write("<h1>" + "El resultado de la suma de las dos variables es: " + "<br>" + sumaTotal + "</h1>");

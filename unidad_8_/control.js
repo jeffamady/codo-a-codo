f9e344562c72c09ed7707877e24ab1719a668169
jeffamady <jeff02.jn@gmail.com>
2021-10-13 19:52:50 -0300

    unit 8

---
+++ unidad_8_/control.js
@@ -0,0 +1,4 @@
+const info = document.querySelector('.info');
+const help = document.querySelector('.help');
+const form = document.querySelector('form');
+const usuario = document.getElementById('usuario')

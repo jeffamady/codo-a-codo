2b8be5b8ba4a0af1ec648881e3d8817b2c8baeed
jeffamady <jeff02.jn@gmail.com>
2021-10-14 15:17:52 -0300

    footer and fontawesome

--- unidad_8_/control.js
+++ unidad_8_/control.js
@@ -1,4 +1,13 @@
 const info = document.querySelector('.info');
 const help = document.querySelector('.help');
 const form = document.querySelector('form');
-const usuario = document.getElementById('usuario')
+const close = document.querySelector('.close');
+const usuario = document.getElementById('usuario');
+const clave = document.getElementById('clave');
+
+
+const closeInfo = () => {
+    info.style.display = 'none';
+}
+close.onclick = closeInfo;
+
